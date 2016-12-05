13aab0e7413c70af47da65edf872b54e14a59ad7
sarahfeldmann <sarahf95@gmail.com>
2016-12-05 02:55:37 -0800

    Add simple map of Seattle

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 'use strict'
 
+var mymap = L.map('mapid').setView([47.6062, -122.3321], 10);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'sarahf95.29p713kp',
+    accessToken: 'pk.eyJ1Ijoic2FyYWhmOTUiLCJhIjoiY2l3YndsZTB5MDRvOTJ0bGtvazZvdGZsYiJ9.gBaTfDLpBwuLDxMfsBBxjw'
+}).addTo(mymap);
